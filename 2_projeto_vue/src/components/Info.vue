6ede832586d93b4804853a790c6aacb489a094db
aribeiro26 <73800428+aribeiro26@users.noreply.github.com>
2022-08-14 21:47:09 -0300

    vshow - diretivas do vue

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,16 +1,26 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades.</p>
+        <p v-show="mostrar_email">Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>JavaScript</li>
             <li>PHP</li>
             <li>Python</li>
         </ul>
+        <p>Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 <script>
 export default {
     name: "Info",
+
+    data() {
+        return {
+            esta_trabalhando: true,
+            mostrar_email: true,
+            email: "alex@alex.com.br",
+        };
+    },
 };
 </script>
