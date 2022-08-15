9dd0ac6df371dad74f6a3e6207bc38447132a233
aribeiro26 <73800428+aribeiro26@users.noreply.github.com>
2022-08-14 21:38:15 -0300

    submit, input, repromto de componentes  componente

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <PrimeiroComponente/>
+    <div>
+        <PrimeiroComponente />
+        <LifeCycle />
+        <Pessoa/>
+    </div>
 </template>
 
 <script>
-  import PrimeiroComponente from './components/PrimeiroComponente.vue';
-  export default{
-    name: 'App',
-    components:{
-      PrimeiroComponente
-    }
-  }
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
+import Pessoa from "./components/Pessoa.vue";
+
+export default {
+    name: "App",
+    components: {
+        PrimeiroComponente,
+        LifeCycle,
+        Pessoa
+    },
+};
 </script>
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>
+            Meu Nome é: {{nome}}
+        </h1>
+    </div>
+</template>
+<script>
+export default{
+    name: 'LifeCycle',
+    data(){
+        return{
+            nome: 'Ainda não sei'
+        }
+    },
+    created(){
+        setTimeout(()=>{
+            this.nome = "Alex"
+        }, 1000)
+        
+    },
+    mounted(){
+          setTimeout(()=>{
+            this.nome = "Ribeiro"
+        }, 2000)
+        
+       
+    }
+}
+</script>
