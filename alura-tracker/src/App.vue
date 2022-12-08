dc007dda22686cd0a9fee70ff3559ee6c3a6fe1d
aribeiro26 <alexetec.ribeiro@gmail.com>
2022-12-08 01:54:38 -0300

    temporizador criado, componentes separados

---
+++ alura-tracker/src/App.vue
@@ -0,0 +1,38 @@
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+ <main class="columns is-gapless is-multiline">
+  <div class="column is-one-quarter">
+    <BarraLateral/>
+  </div>
+   <div class="column is-tree-quarter">
+    <Formulario/>
+  </div>
+ </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    Formulario
+  }
+  
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;  
+  /* margin-top: 60px; */
+}
+</style>
---
+++ alura-tracker/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@fortawesome/fontawesome'
+
+createApp(App).mount('#app')
